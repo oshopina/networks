0cbc156c916712de6acacb1786f32f0fdda7ae8f
Olga Shopina <uqoshopi@uq.edu.au>
2023-05-31 14:23:15 +1000

    Added script to filter fungi from the data

---
+++ Scripts/Filter_fungi_from_18S.R
@@ -0,0 +1,13 @@
+##Filter fungi from taxonomy 
+
+##libraries
+library(dplyr)
+
+##load data 
+otu_with_tax = read.csv2('Data/otu_18S_4350_with_tax_Kenneth_update.csv', header = T, sep = ',', 
+                             row.names = 1)
+otu_with_tax = otu_with_tax[,c(1:141)]
+
+otu_without_fungi = otu_with_tax %>% filter(!grepl('Fungi', taxonomy))
+
+write.csv2(otu_without_fungi, 'otu_18S_4350_without_fungi.csv')
