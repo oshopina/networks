594992ef5c3ac17b602fdbda86120cf9682259c7
Olga Shopina <uqoshopi@uq.edu.au>
2023-11-02 16:53:24 +1000

    Working on clean spiec-easy script

--- Scripts/spiec_easy.R
+++ Scripts/spiec_easy.R
@@ -1,1 +1,9 @@
-bacteria = read.csv()
+bacteria = read.csv('Data/otu_16S_4500.RA.csv')
+fungi = read.csv('Data/otu_ITS_7150.RA.csv')
+rownames(bacteria) = bacteria$X
+rownames(fungi) = fungi$X
+
+bacteria = bacteria[,-1]
+fungi = fungi[,-1]
+
+fungi = fungi[,colnames(bacteria),]
